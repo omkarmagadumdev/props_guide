28d01f36f636d877246761a89c90d6ace6a505c5
Omkar Magadum <omkar.magadum.dev@gmail.com>
2026-04-07 15:15:50 +0000

    feat: add useThemeContext for theme management and wrap App in provider

---
+++ src/context/useThemeContext.js
@@ -0,0 +1,4 @@
+import {createContext} from "react"
+
+export const useThemeContext = createContext(null);
+export const  useAuthContext = createContext(null);
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { useThemeContext } from './context/useThemeContext.js'
 
 createRoot(document.getElementById('root')).render(
+
+  <useThemeContext.Provider>
+
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+
+  </useThemeContext.Provider>
 )
